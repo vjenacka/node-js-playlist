1b57f38f715a608f73403ba79ac7ee6d38cdd833
vjenacka <edins613@gmail.com>
2019-09-15 23:49:53 +0200

    added mongodb with cloud storage and added functionallity

--- controllers/todoController.js
+++ controllers/todoController.js
@@ -1,23 +1,45 @@
 const bodyParser = require("body-parser");
-let data = [
+const mongoose = require("mongoose");
+//connecting to database
+mongoose.connect(
+  "mongodb+srv://test:test@cluster0-cda8f.mongodb.net/test?retryWrites=true&w=majority",
+  { useUnifiedTopology: true, useNewUrlParser: true }
+);
+//creating a schema
+const todoSchema = new mongoose.Schema({
+  item: String
+});
+//creating a database model from a schema
+const Todo = mongoose.model("Todo", todoSchema);
+
+/* let data = [
   { item: "buy milk" },
   { item: "make bed" },
   { item: "one more item" }
-];
+]; */
 const urlencodedParser = bodyParser.urlencoded({ extended: false });
 
 module.exports = function(app) {
   app.get("/todo", (req, resp) => {
-    resp.render("todo", { todos: data });
+    Todo.find({}, (err, data) => {
+      if (err) throw err;
+      resp.render("todo", { todos: data });
+    });
   });
   app.post("/todo", urlencodedParser, (req, resp) => {
-    data.push(req.body);
-    resp.json(data).send();
+    let newTodo = Todo(req.body).save((err, data) => {
+      if (err) throw err;
+      resp.json(data);
+    });
   });
   app.delete("/todo/:item", (req, resp) => {
-    data = data.filter(
-      todo => todo.item.replace(/ /g, "-") !== req.params.item
-    );
-    resp.json(data);
+    Todo.find({ item: req.params.item.replace(/\-/g, " ") }).deleteOne(function(
+      err,
+      data
+    ) {
+      if (err) throw err;
+
+      res.json({ todos: data });
+    });
   });
 };
