054c2f490c73b178d98be6e0ae403848ed5332af
vjenacka <edins613@gmail.com>
2019-09-15 22:31:36 +0200

    added a post and delete route and functionallity with jquery

--- controllers/todoController.js
+++ controllers/todoController.js
@@ -1,7 +1,23 @@
+const bodyParser = require("body-parser");
+let data = [
+  { item: "buy milk" },
+  { item: "make bed" },
+  { item: "one more item" }
+];
+const urlencodedParser = bodyParser.urlencoded({ extended: false });
+
 module.exports = function(app) {
   app.get("/todo", (req, resp) => {
-    resp.render("todo");
+    resp.render("todo", { todos: data });
   });
-  app.post("/todo", (req, resp) => {});
-  app.delete("/todo", (req, resp) => {});
+  app.post("/todo", urlencodedParser, (req, resp) => {
+    data.push(req.body);
+    resp.json(data).send();
+  });
+  app.delete("/todo/:item", (req, resp) => {
+    data = data.filter(
+      todo => todo.item.replace(/ /g, "-") !== req.params.item
+    );
+    resp.json(data);
+  });
 };
